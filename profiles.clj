4aaab81095f525f161b1fb76af91a617d869fcb3
Hugo Duncan <hugo@palletops.com>
2014-03-20 21:52:14 -0400

    Add travis and release configuration

--- profiles.clj
+++ profiles.clj
@@ -3,7 +3,11 @@
        :checkout-deps-shares ^:replace [:source-paths :test-paths
                                         :compile-path]
        :plugins [[codox/codox.leiningen "0.6.4"]
-                 [lein-marginalia "0.7.1"]]}
+                 [lein-marginalia "0.7.1"]
+                 [lein-pallet-release "0.1.3"]]
+       :pallet-release
+       {:url "https://pbors:${GH_TOKEN}@github.com/pallet/pallet-aws.git",
+        :branch "master"}}
  :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
        :codox {:writer codox-md.writer/write-docs
                :output-dir "doc/0.1/api"
@@ -13,8 +17,7 @@
                  "codox" ["doc"]
                  "doc" ["do" "codox," "marg"]}}
  :release
- {:plugins [[lein-set-version "0.2.1"]]
-  :set-version
+ {:set-version
   {:updates [{:path "README.md" :no-snapshot true}]}}
  :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
  :clojure-1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
