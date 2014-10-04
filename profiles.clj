34a1f671bd3b073285ff83584206758f2c684379
Hugo Duncan <hugo@hugoduncan.org>
2014-10-04 08:26:35 -0400

    Update release configuration

--- profiles.clj
+++ profiles.clj
@@ -2,23 +2,7 @@
                       [org.slf4j/jcl-over-slf4j "1.7.6"]]
        :checkout-deps-shares ^:replace [:source-paths :test-paths
                                         :compile-path]
-       :plugins [[codox/codox.leiningen "0.6.4"]
-                 [lein-marginalia "0.7.1"]
-                 [lein-pallet-release "RELEASE"]]
-       :pallet-release
-       {:url "https://pbors:${GH_TOKEN}@github.com/pallet/pallet-aws.git",
-        :branch "master"}}
- :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
-       :codox {:writer codox-md.writer/write-docs
-               :output-dir "doc/0.1/api"
-               :src-dir-uri "https://github.com/pallet/pallet-aws/blob/develop"
-               :src-linenum-anchor-prefix "L"}
-       :aliases {"marg" ["marg" "-d" "doc/0.1/"]
-                 "codox" ["doc"]
-                 "doc" ["do" "codox," "marg"]}}
- :release
- {:set-version
-  {:updates [{:path "README.md" :no-snapshot true}]}}
+       :plugins [[lein-pallet-release "RELEASE"]]}
  :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
  :clojure-1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
  :clojure-1.6 {:dependencies [[org.clojure/clojure "1.6.0-RC.1"]]}}
