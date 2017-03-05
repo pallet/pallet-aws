24767836575b2dc2723a4b9618f83c503d40efb6
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-03-05 12:21:31 +0100

    release 0.2.8.2 able to connect to eu-central

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.domaindrivenarchitecture/pallet-aws "0.2.8"
+(defproject org.domaindrivenarchitecture/pallet-aws "0.2.8.2"
   :description "A provider for using Pallet with AWS EC2, based on the AWS SDK."
   :url "http://palletops.com"
   :license {:name "All rights reserved"}
   :scm {:url "git@github.com:pallet/pallet-aws.git"}
-  :dependencies [[com.palletops/pallet-aws-ops "0.2.2"
+  :dependencies [[org.domaindrivenarchitecture/pallet-aws-ops "0.2.3.1"
                   :exclusions [commons-logging]]
                  [com.palletops/pallet "0.8.12"]
                  [org.clojure/core.match "0.2.2"
